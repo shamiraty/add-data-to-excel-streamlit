204147c14f9cdd7e809ce533672f5e5a49e512f3
shamiraty <129072179+shamiraty@users.noreply.github.com>
2023-05-20 21:41:17 +0300

    Add files via upload

---
+++ template.py
@@ -0,0 +1,30 @@
+import streamlit as st
+
+def UI():
+    st.markdown("""  
+            
+ <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+<div class="container-fluid">
+    <div class="card shadow">
+        <div class="card-body">
+        
+<div class="card">
+  <div class="card-body">
+    <h3 class="text-secondary">⚛  Add Data to Excel File | Data Visualization</h3>
+  </div>
+</div>
+
+
+</div>
+</div>
+</div>
+ 
+
+<style>
+div[data-testid="stExpander"] div[role="button"] p
+{
+    font-size: 1.1rem;
+}
+ 
+</style>
+ """, unsafe_allow_html=True)
